e7b2c40c022f79ebcb914108bda2f0a84f2ed1b6
arevalo <arevalo@gonzalez>
2019-06-22 13:48:00 -0600

    Test a ordenRest

---
+++ src/test/java/com/dsi2019/ues/fmocc/ingenieria/dsi2019/boundary/service/OrdenRestTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dsi2019.ues.fmocc.ingenieria.dsi2019.boundary.service;
+
+import com.dsi2019.ues.fmocc.ingenieria.dsi2019.controller.OrdenFacade;
+import com.dsi2019.ues.fmocc.ingenieria.dsi2019.entity.Orden;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Matchers.any;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mockito.internal.util.reflection.Whitebox;
+
+/**
+ *
+ * @author arevalo
+ */
+public class OrdenRestTest {
+    OrdenREST ordenRest= mock(OrdenREST.class);
+    OrdenFacade ordenFacade=mock(OrdenFacade.class);
+    Orden orden=mock(Orden.class);
+    List<Orden> list= new ArrayList<>();
+    @Before
+    public void init(){
+        when(ordenFacade.existe(any(Integer.class))).thenReturn(Boolean.TRUE);
+        Whitebox.setInternalState(ordenRest, "ordenFacade", ordenFacade);
+        orden= new Orden(1);
+        list.add(orden);
+        list.add(orden);
+        list.add(orden);
+    }
+    @Test
+    public void editTest(){
+        doCallRealMethod().when(ordenRest).edit(any(Integer.class), any(Orden.class));
+        assertEquals(200,ordenRest.edit(1,orden).getStatus());
+        assertEquals("[Registro Modificado]", ordenRest.edit(1, orden).getHeaders().keySet().toString());
+        
+        assertEquals(404,ordenRest.edit(1,null).getStatus());
+        assertEquals("[Error al Modificar]", ordenRest.edit(1, null).getHeaders().keySet().toString());
+        
+    }
+    
+    @Test
+    public void findAllTest(){
+        doCallRealMethod().when(ordenRest).findAll();
+        when(ordenFacade.find()).thenReturn(list);
+        
+        assertEquals(200, ordenRest.findAll().getStatus());
+        assertEquals("[Total-Reg]", ordenRest.findAll().getHeaders().keySet().toString());
+        
+    }
+    
+    @Test
+    public void FindRangeTest(){
+        
+    }
+    
+}
---
+++ src/test/java/com/dsi2019/ues/fmocc/ingenieria/dsi2019/boundary/service/ParametroRestTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dsi2019.ues.fmocc.ingenieria.dsi2019.boundary.service;
+
+/**
+ *
+ * @author arevalo
+ */
+public class ParametroRestTest {
+    
+}
